b9796fc130e6c976c73156437cf3ddbb08c7ba45
Marmar9999 <119695675+Marmar9999@users.noreply.github.com>
2024-07-09 01:10:15 +0300

    edit session three assignments

--- JavaFundamentals/SessionThree/Amira_Mahmoud_Nayel_S1/SessionThree.java
+++ JavaFundamentals/SessionThree/Amira_Mahmoud_Nayel_S1/SessionThree.java
@@ -4,6 +4,7 @@ class SessionThree{
     //Print from 1 to 10: 
         System.out.println("\n1) Print from 1 to 10: ");
         for(int i =1 ; i<=10; i++){System.out.print(i + ", ");}
+        
     //Print from 10 to users num:
         System.out.print("\n\n2) Print from 10 to: ");
         int x = new Scanner(System.in).nextInt();
